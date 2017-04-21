9cf2882f3e4c32d890934f1254bda934477d102b
satie <satiesharma@gmail.com>
2017-04-21 13:44:12 -0400

    Updates to fix concurrency issues

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 waf-update-blacklist
 ====================
+AWS Lambda function to create and update [WAF IPSets](http://docs.aws.amazon.com/waf/latest/APIReference/API_IPSet.html) with entries in a DynamoDB database of blacklisted IP addresses created with [waf-update-ipdatabase](https://github.com/hcentive/waf-update-ipdatabase).
--- index.js
+++ index.js
@@ -9,16 +9,39 @@ var db = require('./lib/dynamodb.js');
 // fw.getSignatures(1, null, callback);
 // fw.getIPBlacklists(1, null, callback);
 
+// exports.handler = function(event, context) {
 var updateFirewall = function(callback) {
   db.getActiveIPRecords(function (err, iprecords) {
     if (err) {
-      cback(err, null);
+      console.log(err, null);
     } else {
-      if (iprecords.Items) {        
+      console.log(iprecords.Items.length + " active records in IP database");
+      if (iprecords.Items) {
         var groupedBySrc = _.groupBy(iprecords.Items, 'SourceRBL');
-        var addrMap = _.map(groupedBySrc, function(val, key) { return [key, val.map(function(o) { return o.IPAddress; })];});
-        addrMap.forEach(function (addr) {
-          fw.updateBlacklists(addr[1], addr[0], callback);
+        var addrMap = _.map(groupedBySrc, function(val, key) { return [key, val.map(function(o) {
+          if (o.IPAddress.indexOf('/') === -1) {
+            return o.IPAddress + '/32';
+          } else {
+            return o.IPAddress;
+          }
+        })];});
+
+        async.eachSeries(addrMap, function(addr, callback) {
+          // console.log(addr[0]);
+          fw.updateBlacklists(addr[1], addr[0], (e, r) => {
+            if (e) {
+              callback(e, null);
+            } else {
+                callback();
+            }
+          });
+        },
+        function (err) {
+          if (err) {
+            console.log(err, err.stack)
+          } else {
+            console.log("Done!");
+          }
         });
       }
     }
@@ -34,14 +57,3 @@ function callback(err, data) {
     console.log(data);
   }
 }
-
-// exports.handler = function(event, context) {
-
-  // waf.getIPSet({IPSetId: config.ipSetId}, function(err, ipset){
-  //   if (err != null) {
-  //     console.log(err);
-  //     return;
-  //   }
-  //   updateAVBlacklist(config.otx_start_path, ipset);
-  // });
-// };
